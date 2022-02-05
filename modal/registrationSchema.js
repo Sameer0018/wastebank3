6f769bd06f6f627fe137ebc1b5b4b98c89dcfdc9
Sameer Hussain <87130217+Sameer0018@users.noreply.github.com>
2022-02-05 17:06:15 +0530

    Update registrationSchema.js

--- modal/registrationSchema.js
+++ modal/registrationSchema.js
@@ -23,10 +23,6 @@ const registrationSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-      coins: {
-        type: Number,
-        required: true
-    },
        
     date: {
         type: Date,
